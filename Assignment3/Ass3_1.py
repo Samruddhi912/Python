06a648e4f73a96812eeb3df726e380ddf82d5302
Samruddhi912 <146476526+Samruddhi912@users.noreply.github.com>
2025-05-15 13:05:13 +0530

    Ass3_1.py

--- Assignment3/Ass3_1.py
+++ Assignment3/Ass3_1.py
@@ -1,1 +1,21 @@
+def Add(List):
+    sum=0
+    for i in List:
+        sum=sum+i
+    return sum
+
+def main():
+    Data=[]
+    print("Enter number of elements you want to enter:")
+    num=int(input())
+    print("Enter the elements:")
+    for i in range(num):
+        no=int(input())
+        Data.append(no)
+    Res=Add(Data)
+    print("Addition of list is",Res)
+
+
+if __name__=="__main__":
+    main()
 

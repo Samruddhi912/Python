42d319a1ac9b147e0be024fa765eb125062ea79a
Samruddhi912 <146476526+Samruddhi912@users.noreply.github.com>
2025-06-20 16:58:04 +0530

    Ass20_1.py

--- Assignment20/Ass20_1.py
+++ Assignment20/Ass20_1.py
@@ -1,1 +1,46 @@
+import sys
+import os
+import hashlib
 
+def CheckSum(path,BlockSize=1024):
+    fobj=open(path,"rb")
+
+    hobj=hashlib.md5()
+    buffer=fobj.read(BlockSize)
+    while (len(buffer)>0):
+        hobj.update(buffer)
+        buffer=fobj.read(BlockSize)
+
+    fobj.close()
+    return hobj.hexdigest()
+
+def DirectoryCheckSum(DName):
+    flag=os.path.isabs(DName)
+    if flag==False:
+        DName=os.path.abspath(DName)
+
+    flag=os.path.exists(DName)
+    if flag==False:
+        print("Invalid Path")
+        exit()
+    
+    flag=os.path.isdir(DName)
+    if flag==False:
+        print("Not directory")
+        exit()
+    
+    for Folder,SubFolders,Files in os.walk(DName):
+        for FNames in Files:
+            FNames=os.path.join(Folder,FNames)
+            checksum=CheckSum(FNames)
+            print("File name is:",FNames)
+            print("CheckSum is:",checksum)
+            print()
+
+def main():
+    DirName=sys.argv[1]
+    DirectoryCheckSum(DirName)
+
+
+if __name__=="__main__":
+    main()

adf327bbeb9932feccb0edfe6fa3f073615368df
Samruddhi912 <146476526+Samruddhi912@users.noreply.github.com>
2025-06-22 14:04:46 +0530

    Update Ass21_1.py

--- Assignment21/Ass21_1.py
+++ Assignment21/Ass21_1.py
@@ -1,1 +1,19 @@
 
+import psutil
+
+def procInfo():
+    Border="-"*80
+    print(Border)
+    print("Information of running processes")
+    print(Border)
+    for proc in psutil.process_iter():
+        info=proc.as_dict(attrs=['name','pid','username'])
+        info['vms']=proc.memory_info().vms/(1024*1024)
+        print(info)
+
+def main():
+    procInfo()
+    
+
+if __name__=="__main__":
+    main()
